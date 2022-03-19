5d61803b7276c3164f9eb91c6a97f60a1b05250d
Jhonatan Mariani <jhonatan_mariani@edu.univali.br>
2022-03-19 17:04:26 -0300

    Alt Main.java

--- implementacao-compiladores/src/main/java/Main.java
+++ implementacao-compiladores/src/main/java/Main.java
@@ -1,6 +1,16 @@
+import classes.Language20221;
+import org.javacc.parser.ParseException;
+import org.javacc.parser.Token;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Ola imundo");
+    public static void main(String[] args) throws ParseException {
+        Language20221 language20221 = new Language20221(System.in);
+        language20221.getTokens(args, "123");
+        //System.out.println(language20221.getResults());
     }
 }
