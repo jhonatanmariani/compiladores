c862c249410f66d2f4dc41beab8b8041778d231b
Jhonatan Mariani <jhonatan_mariani@edu.univali.br>
2022-03-18 22:09:40 -0300

    Add .bat gera classes Java do arquivo .jj

---
+++ geraClassesJavaJJ.bat
@@ -0,0 +1,5 @@
+echo "Executando o script de geração das classes Java do analisador a partir do arquivo .jj"
+cd implementacao-compiladores\src\main\java\classes-analisador-lexico\
+echo "Diretório que seram geradas/armazenadas as classes Java: " %CD%
+javacc ../linguagem20221.jj
+echo "Classes Java geradas!"
