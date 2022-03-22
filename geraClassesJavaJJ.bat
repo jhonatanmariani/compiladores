699c30e8923aa570ef5b3865dd697c1e9f64ff4b
Jhonatan Mariani <jhonatan_mariani@edu.univali.br>
2022-03-21 22:26:01 -0300

    Add script gera .jar & Alt script gera classes java .jj

--- geraClassesJavaJJ.bat
+++ geraClassesJavaJJ.bat
@@ -1,6 +1,6 @@
 echo "Executando o script de geração das classes Java do analisador a partir do arquivo .jj"
 cd implementacao-compiladores\src\main\java\classes\
 echo "Diretório que seram geradas/armazenadas as classes Java: " %CD%
-javacc linguagem20221.jj
+CALL javacc linguagem20221.jj
 echo "Classes Java geradas!"
-pause
+PAUSE
